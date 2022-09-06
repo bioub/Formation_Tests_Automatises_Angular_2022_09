78a675f8524fc71cc26de8aa5e1a4e2b5a7bd01d
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-09-06 17:16:02 +0200

    Slides à jour

--- angular-exercices/cypress/e2e/users.cy.ts
+++ angular-exercices/cypress/e2e/users.cy.ts
@@ -1,18 +1,20 @@
 describe('Users', () => {
   it('should display users from backend', () => {
-  //  cy.intercept({
-  //     method: 'GET',
-  //     url: 'https://jsonplaceholder.typicode.com/users',
-  //   }, {
-  //     statusCode: 200,
-  //     body: [{id: 1, name: 'Toto'}, {id: 2, name: 'Titi'}],
-  //     headers: { 'access-control-allow-origin': '*' },
-  //     // delayMs: 2000,
-  //   }).as('getUsers')
+   cy.intercept({
+      method: 'GET',
+      url: 'https://jsonplaceholder.typicode.com/users',
+    }, {
+      statusCode: 200,
+      body: [{id: 1, name: 'Toto'}, {id: 2, name: 'Titi'}],
+      headers: { 'access-control-allow-origin': '*' },
+      // delayMs: 2000,
+    }).as('getUsers')
 
     cy.visit('/users');
 
-    cy.contains('Leanne');
-    // cy.contains('Titi');
+    // cy.wait('@getUsers')
+
+    cy.contains('Toto');
+    cy.contains('Titi');
   })
 })
